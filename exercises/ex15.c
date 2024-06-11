c1ba0afab69b7b40e34f1a3d6a2ea56c8313b42e
Zicheng_Liang <liangzicheng02@gmail.com>
2024-06-11 11:12:15 +0800

    ex16 completed, still exploring the extra credits

--- exercises/ex15.c
+++ exercises/ex15.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main (int argc, char *argv[])
 {
     // create two arrays we care about
@@ -18,8 +17,9 @@ int main (int argc, char *argv[])
     int i = 0;
 
     // first way using indexing
-    for (i = 0; i < count; i++){
-        printf("%s has %d years alive.\n", names[i], ages[i]);
+    while (i < count){
+        ("%s has %d years alive.\n", names[i], ages[i]);
+        i++;    
     }
     printf("---\n");
 
@@ -28,7 +28,7 @@ int main (int argc, char *argv[])
     char **cur_name = names; // this is a pointer to pointers, as *names[] is an array of pointers
 
     // second way using pointers
-    for (i = 0; i < count; i++){
+    for (i = count - 1; i >= 0;  i--){
         printf("%s is %d years old.\n", 
                 *(cur_name + i), *(cur_age + i));
         // when using a pointer, it gives you the value in that address
@@ -38,7 +38,7 @@ int main (int argc, char *argv[])
     printf("--\n");
 
     // third way, pointers are just arrays
-    for (i = 0; i < count; i++){
+    for (i = count - 1; i >= 0; i--){
         printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
         // you can also pretend pointers were arrays
         // you are actually telling the compiler to jump i places and return the value there
@@ -47,8 +47,9 @@ int main (int argc, char *argv[])
     printf("---\n");
 
     //fourth way with pointers in a stupid complex way
-    for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++){
+    for (cur_name = &names[4], cur_age = ages; cur_age >= ages; cur_name--, cur_age--){
         printf("%s is lived %d years so far.\n",  *cur_name, *cur_age);
     }
     return 0;
 }
+
