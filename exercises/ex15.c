aa212e5a513e067b162baf4e77eb7ace2589d3ed
Zicheng_Liang <liangzicheng02@gmail.com>
2024-06-17 08:39:00 +0800

    Done with all extra credits from ex15 and 16

--- exercises/ex15.c
+++ exercises/ex15.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void printAll (char **name, int *age, int count);
 int main (int argc, char *argv[])
 {
     // create two arrays we care about
@@ -14,28 +15,29 @@ int main (int argc, char *argv[])
 
     // safely get the size of ages
     int count = sizeof(ages) / sizeof(int); // I'm amazed by how to get the array's length in C
+    printf("the size of the int array is %d\n", count);
     int i = 0;
 
-    // first way using indexing
+    // first way using array indexing
     while (i < count){
-        ("%s has %d years alive.\n", names[i], ages[i]);
+        printf("%s has %d years alive.\n", names[i], ages[i]);
         i++;    
     }
     printf("---\n");
 
     // set up the pointers to the start of the arrays
     int *cur_age = ages; // it points to where ages[] start
-    char **cur_name = names; // this is a pointer to pointers, as *names[] is an array of pointers
+    char **cur_name = &names[0]; // this is a pointer to pointers, as *names[] is an array of pointers
 
     // second way using pointers
-    for (i = count - 1; i >= 0;  i--){
+    for (i = 0; i < count; i++) {
         printf("%s is %d years old.\n", 
-                *(cur_name + i), *(cur_age + i));
-        // when using a pointer, it gives you the value in that address
-        // the advantage of pointers is that they can be moved around
+                *(cur_name + i), *(cur_age +i));
     }
+    // when using a pointer by *ptr, it gives you the VALUE in that address
+    // the advantage of pointers is that they can be moved around
 
-    printf("--\n");
+    printf("---\n");
 
     // third way, pointers are just arrays
     for (i = count - 1; i >= 0; i--){
@@ -47,9 +49,22 @@ int main (int argc, char *argv[])
     printf("---\n");
 
     //fourth way with pointers in a stupid complex way
-    for (cur_name = &names[4], cur_age = ages; cur_age >= ages; cur_name--, cur_age--){
-        printf("%s is lived %d years so far.\n",  *cur_name, *cur_age);
+    for(cur_age = ages, cur_name = names; (cur_age - ages) < count; cur_age++, cur_name++) {
+        printf("%s lived %d years so far\n", *cur_name, *cur_age);
     }
+
+    cur_name = &names[0];
+    cur_age = &ages[0];
+    printAll(cur_name, cur_age, count);
+
     return 0;
 }
 
+void printAll (char **name, int *age, int count){
+    printf("---\n");
+    printf("Now we use a function to print out:\n");
+    for (int i = 0; i < count; i++) {
+    printf("%s has lived %d years\n", *(name + i), *(age + i));
+    printf("now the pointer name points to %p, age points to %p\n", name + i, age + i);
+    }
+}
--- exercises/ex16.c
+++ exercises/ex16.c
@@ -10,58 +10,48 @@ struct Person {
     int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight){
-    struct Person *who = malloc(sizeof(struct Person));
-    assert(who != NULL);
+struct Person Person_create(char *name, int age, int height, int weight){
+    // return a pointer typed as Person
+    // allocate a chunk of memory with the size of Person, and create a pointer to the structrue
+    // error check
+    struct Person who; 
+    who.name = name;
+    who.age = age;
+    who.height = height;
+    who.weight = weight;
 
-    who->name = strdup(name);
-    who->age = age;
-    who->height = height;
-    who->weight = weight;
-
+    // what is stdrup? here it is used to retrive the value of pointer name;
+    // who -> ... can just be considered as *(ptr + 1) but with mutable pointer type;
     return who;
 }
 
-void Person_destroy(struct Person *who){
-    assert(who != NULL);
-
+void Person_destroy(struct Person who){
     //free(who->name);
-    free(who);
 }
 
-void Person_print(struct Person *who){
-    printf("Name: %s\n", who->name);
-    printf("\tAge: %d\n", who->age);
-    printf("\tHeight: %d\n", who->height);
-    printf("\tWeight: %d\n", who->weight);
+void Person_print(struct Person who){
+    printf("Name: %s\n", who.name);
+    printf("\tAge: %d\n", who.age);
+    printf("\tHeight: %d\n", who.height);
+    printf("\tWeight: %d\n", who.weight);
 }
 
 int main(int argc, char *argv[]){
     // make two people structures
-    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
-    struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+    struct Person joe = Person_create("Joe Alex", 32, 64, 140);
+    struct Person frank = Person_create("Frank Blank", 20, 72, 180);
 
     // print them out and where they are in memory
-    // %p is the format for printing out an address
-    printf("Joe is at memory location: %p:\n", joe);
     Person_print(joe);
-
-    printf("Frank is at memory location: %p:\n", frank);
     Person_print(frank);
+    // %p is the format for printing out an address
+
 
     // make everyone age 20 years and print them again
-    joe->age += 20;
-    joe->height -= 2;
-    joe->weight += 40;
+    joe.age += 20;
+    frank.age += 20;
+    
     Person_print(joe);
-
-    frank->age += 20;
-    frank->weight += 20;
     Person_print(frank);
-
-    // destroy them both so we clean up
-    Person_destroy(joe);
-    Person_destroy(frank);
-
     return 0;
 }
